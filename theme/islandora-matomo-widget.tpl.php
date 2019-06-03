6f868f7dfdefff43f4b0dd1dc793be47304a9fa9
Diego Pino Navarro <dpino@metro.org>
2019-06-03 13:55:18 -0400

    Matomo refactor for LASIR
    Adds blocks, dimensions, events, new config page. WIP

---
+++ theme/islandora-matomo-widget.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * islandora-matomo-widget.tpl.php
+ *
+ */
+?>
+<div class="islandora-matomo-widget">
+  <div id="widgetIframe">
+
+      <iframe width="100%" height="350" src="<?php print $matomo_iframe_url; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0">
+      </iframe>
+  </div>
+  <div id="widgetIframe2">
+
+    <iframe width="100%" height="350" src="<?php print $matomo_iframe_url2; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0">
+    </iframe>
+  </div>
+</<div>
