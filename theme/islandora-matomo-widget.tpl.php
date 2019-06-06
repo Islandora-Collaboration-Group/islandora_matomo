2bba9bee866e976e72f5f7cd0eb4b948ceea4098
Diego Pino Navarro <dpino@metro.org>
2019-06-06 13:53:44 -0400

    Add scolling because iframes are the worst (ever)

--- theme/islandora-matomo-widget.tpl.php
+++ theme/islandora-matomo-widget.tpl.php
@@ -7,13 +7,11 @@
 ?>
 <div class="islandora-matomo-widget">
   <div id="widgetIframe">
-
-      <iframe width="100%" height="350" src="<?php print $matomo_iframe_url; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0">
+      <iframe width="100%" height="350" src="<?php print $matomo_iframe_url; ?>" frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
       </iframe>
   </div>
   <div id="widgetIframe2">
-
-    <iframe width="100%" height="450" src="<?php print $matomo_iframe_url2; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0">
+    <iframe width="100%" height="450" src="<?php print $matomo_iframe_url2; ?>"  frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
     </iframe>
   </div>
 </<div>
