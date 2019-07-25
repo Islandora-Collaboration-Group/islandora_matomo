1d1161096a6505fb6fda13e846aa387809676d5a
Diego Pino Navarro <dpino@metro.org>
2019-07-25 12:05:31 -0400

    Give the widget a title

--- theme/islandora-matomo-widget.tpl.php
+++ theme/islandora-matomo-widget.tpl.php
@@ -7,6 +7,7 @@
 ?>
 <?php if ($matomo_token): ?>
 <div class="islandora-matomo-widget">
+  <h3>Visit Map for this Digital Object during the last 30 days</h3>
   <div id="widgetIframe">
       <iframe width="100%" height="350" src="<?php print $matomo_iframe_url; ?>" frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
       </iframe>
