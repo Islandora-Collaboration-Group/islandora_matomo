fffc09d7d5cd7b4e8eaf8916e669f2afe4fb21a2
Diego Pino Navarro <dpino@metro.org>
2019-07-25 12:09:37 -0400

    And another title

--- theme/islandora-matomo-widget.tpl.php
+++ theme/islandora-matomo-widget.tpl.php
@@ -7,14 +7,15 @@
 ?>
 <?php if ($matomo_token): ?>
 <div class="islandora-matomo-widget">
-  <h5>Mapped visits during the last 30 days for this resource</h5>
   <div id="widgetIframe">
-      <iframe width="100%" height="350" src="<?php print $matomo_iframe_url; ?>" frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
-      </iframe>
+      <h5>Mapped visits during the last 30 days for this resource</h5>
+        <iframe width="100%" height="350" src="<?php print $matomo_iframe_url; ?>" frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
+        </iframe>
   </div>
   <div id="widgetIframe2">
-    <iframe width="100%" height="450" src="<?php print $matomo_iframe_url2; ?>"  frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
-    </iframe>
+      <h5>Real time visits Map (24 hours)</h5>
+        <iframe width="100%" height="460" src="<?php print $matomo_iframe_url2; ?>"  frameborder="0" marginheight="0" marginwidth="0" scrolling="yes">
+        </iframe>
   </div>
 </<div>
 <?php endif;?>
