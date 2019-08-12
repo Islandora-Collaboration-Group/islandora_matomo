48f392026e155927176cda72caa96ed0c32d3bd3
Diego Pino Navarro <dpino@metro.org>
2019-08-12 15:48:07 -0400

    First (and hopefully last) iteration on Object count block
    A lot of code. Not tested, Hopefully it will just work.
    @noahwsmith I will update your server tomorrow to enable these changes. Hopefully all works.

---
+++ theme/islandora-matomo-count.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * islandora-matomo-widget.tpl.php
+ */
+?>
+<?php if ($matomo_token): ?>
+<div class="islandora-matomo-count">
+    <div class="row stats-box">
+        <div class="row">
+            <div class="col-sm-6">
+                <span class="stats-data"><?php print $stats['visits']; ?></span>
+            </div>
+            <div class="col-sm-6">
+                &nbsp;visits<i class="fa fa-eye " data-toggle="tooltip" tooltip="" data-placement="top" title="" data-original-title="Visits."></i>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <span class="stats-data"><?php print $stats['downloads']; ?></span>
+            </div>
+            <div class="col-sm-6">
+                &nbsp;dowloads<i class="fa fa-download " data-toggle="tooltip" tooltip="" data-placement="top" title="" data-original-title="Downloads."></i>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif;?>
