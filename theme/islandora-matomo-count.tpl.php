8035c3f3a2a7c55637c9573abaf9d37043779145
Diego Pino Navarro <dpino@metro.org>
2019-08-19 13:36:40 -0400

    cleaner CSS

--- theme/islandora-matomo-count.tpl.php
+++ theme/islandora-matomo-count.tpl.php
@@ -7,21 +7,21 @@
 ?>
 <?php if ($matomo_token): ?>
 <div class="islandora-matomo-count">
-    <div class="stats-box">
+    <div class="islandora-matomo-stats-box">
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-3">
                 <span class="stats-data"><?php print $stats['visits']; ?></span>
             </div>
-            <div class="col-sm-6">
-                &nbsp;visits<i class="fa fa-eye " data-toggle="tooltip" tooltip="" data-placement="top" title="" data-original-title="Visits."></i>
+            <div class="col-sm-8">
+                <div class="islandora-matomo-stats-legend">visits</div><i class="fa fa-eye " data-toggle="tooltip" tooltip="" data-placement="top" title="" data-original-title="Visits."></i>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-3">
                 <span class="stats-data"><?php print $stats['downloads']; ?></span>
             </div>
-            <div class="col-sm-6">
-                &nbsp;dowloads<i class="fa fa-download " data-toggle="tooltip" tooltip="" data-placement="top" title="" data-original-title="Downloads."></i>
+            <div class="col-sm-8">
+                <div class="islandora-matomo-stats-legend">dowloads</div><i class="fa fa-download " data-toggle="tooltip" tooltip="" data-placement="top" title="" data-original-title="Downloads."></i>
             </div>
         </div>
     </div>
